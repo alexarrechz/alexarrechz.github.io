0290f1f11726c958068e8e2d4cf2d510491583a7
Elian <eliangerardiso@gmail.com>
2023-05-26 12:07:56 -0600

    Fixed more relative routes

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,10 @@
+import devStar from '../mapa/DevStar.svg';
+
 export const Footer = () => {
     return (
         <footer className="bg-dark text-light border-dark border-bottom border-5 d-flex align-items-center" style={{height: "10vh"}}>
 
-            <img src="mapa/DevStar.svg" width="35" className="d-inline-block pb-2 ms-5 me-3"/>
+            <img src={devStar} width="35" className="d-inline-block pb-2 ms-5 me-3"/>
                 <h3 className="fs-6 mt-1">Equipo Dev y asi</h3>
         </footer>
     );
